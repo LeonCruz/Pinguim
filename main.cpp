62d00dc8353b84db2b44fbda6bb0ba65ea1cca2e
leonCruz <leon.cruz.teixeira@gmail.com>
2018-02-27 09:30:46 -0300

    Alteração no método main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <ctime>
 using namespace std;
 
-int lado1[] = {1,2,3,4,5,6}, gelinho[2], lado2[6];
+// Muda essa variável 'gelinho' pra um nome mais representativo
+int lado1[] = {1, 2, 3, 4, 5, 6}, gelinho[2], lado2[6];
 bool vazio;
 
+// Tem que saber se esse número aleatório é maior que o tamanho do vetor
+// E tem que ter outra função que escolhe quantos pinguins ele vai levar 1 ou 2. 
 int posAleatorio(){
 	int x;
 	srand(time(0));
@@ -45,25 +48,12 @@ int main(int argc, char** argv) {
 	mostrarVetor();
 	cout << "\n\n==================================\n\n";
 	
+	// Aqui tu tem que testar o número de opções, 1 ou 2, 
+    // Se for 1, vai ter que chamar a função que escolhe um pinguim aleatóriamente, se for de acordo com as regras, move o pingum.
+	// Se for 2, vai ter que chamar a função que escolhe dois pinguins aleatóriamente, se for de acordo com as regras, move os pinguins 
 	int posicao = posAleatorio();
-
-	do{
-		if(lado1[posicao]%2==0){
-			gelinho[0] = lado1[posicao];
-			lado1[posicao] = NULL;
-			mostrarVetor();
-			resetGame();
-		}else{
-			gelinho[0] = lado1[posicao];
-			gelinho[1] = lado1[posicao+1];
-			lado1[posicao] = NULL;
-			lado1[posicao+1] = NULL;
-			mostrarVetor();
-			lado2[posicao+1] = gelinho[1];
-			gelinho[1] = NULL;
-			mostrarVetor();
-		}
-	}while(vazio == false);
+	
+	
 	
 	return 0;
 }
